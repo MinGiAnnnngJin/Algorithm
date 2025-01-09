fb12adaf2fe5bc0802c48b22154b3d54f470e466
qt_GyeongJin <103627225+MinGiAnnnngJin@users.noreply.github.com>
2025-01-09 16:30:18 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-      int answer =0;
-      String number = String.valueOf(n);
-      for(int i=0; i<number.length();i++)
-        answer+=Character.getNumericValue(number.charAt(i));
-
-     return answer;
+     int answer = 0;
+     
+    while(n>0) {
+        answer += n %10;
+        n=n/10;
+    }
+        return answer;
 }
 }
