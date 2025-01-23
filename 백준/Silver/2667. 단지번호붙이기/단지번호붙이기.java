964197515f38dc3799c35f3827f6e3f59e4565b3
qt_GyeongJin <103627225+MinGiAnnnngJin@users.noreply.github.com>
2025-01-24 00:45:36 +0900

    [Silver I] Title: 단지번호붙이기, Time: 184 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,68 +4,72 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-   static int N;
-   static int[][] map;
-   static boolean[][] visited;
-   static int[] dx= {-1,1,0,0};
-   static int[] dy= {0,0,-1,1};
-   static List<Integer> result = new ArrayList<>();
+    static int n; //지도 크기
+    static int[][] map; //지도 정보
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int [] dy = {0, 0, -1, 1};
+    static List<Integer> result = new ArrayList<>();
 
-   public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-       N=sc.nextInt();
-       map=new int[N][N];
-       visited = new boolean[N][N];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); // 지도 크기
+        map = new int[n][n];
+        visited = new boolean[n][n];
 
+        //지도 내용물 입력 받기, sc.next는 한줄로 입력받아짐
+        for (int i = 0; i < n; i++) {
+            String line = sc. next();
+            for (int j = 0; j < n; j++) {
+                map[i][j] =line.charAt(j)-'0';
+            }
+        }
 
-       //지도 만들기
-       for(int i=0;i<N;i++) {
-           String line = sc.next();
-           for(int j =0; j<N;j++){
-               map[i][j]=line.charAt(j)-'0';
-           }
-       }
+        // 지도 탐색
+        for(int i= 0; i<n;i++){
+            for(int j=0;j<n;j++){
+                if(map[i][j]==1 && !visited[i][j]){
+                    int size = dfs(i,j);  //단지 크기
+                    result.add(size);
+                }
+            }
+        }
 
-       //위치 탐색
-       for(int i=0; i<N;i++){
-           for(int j=0;j<N;j++){
-               if(map[i][j]==1 && !visited[i][j]){
-                   int size = dfs(i,j);  // 단지 크기 계산
-                   result.add(size);
-               }
-           }
-       }
+        //리스트 정렬
+        Collections.sort(result);
+        //단지수 출력
+        System.out.println(result.size());
+        for(int i =0; i<result.size();i++){
+            System.out.println(result.get(i));
+        }
+    }
 
-       Collections.sort(result); //단지 크기 오름차순 정렬
-       System.out.println(result.size());  //단지 수
-       for(int size : result){
-           System.out.println(size);  //각 단지 크기
-       }
-   }
-   static int dfs(int x, int y ){
+    static int dfs(int x, int y){
        Stack<int[]> stack = new Stack<>();
-       stack.push(new int[]{x,y});
-       visited[x][y] = true;
-       int count =0;
+       stack.add(new int[] {x,y});
+       visited[x][y]=true;
+       int count = 0;
 
        while(!stack.isEmpty()){
-           int[] current = stack.pop();
+           int [] current = stack.pop(); //현재 정점의 x,y값
            int cx = current[0];
            int cy = current[1];
            count ++;
 
-           //4방향 탐색
-           for(int d=0; d<4; d++){
+           //현재 정점 주변 방문
+           for(int d =0; d < 4; d++){
                int nx = cx + dx[d];
                int ny = cy + dy[d];
-               if(nx >= 0 && ny >= 0 && nx<N && ny<N ){
-                   if(map[nx][ny] ==1 && !visited[nx][ny]){
+
+               if(nx>=0 && ny>=0 && nx<n && ny<n ){  // 범위 체크
+                   if(map[nx][ny]==1 &&!visited[nx][ny]){
                        stack.push(new int[] {nx,ny});
-                       visited[nx][ny] = true;
+                       visited[nx][ny]=true;
                    }
                }
+
            }
-       }
-       return count;
-   }
+              }
+        return count;
+    }
 }
